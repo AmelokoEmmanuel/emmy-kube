cab07fa45937d1e6a7718e72fd765cc37f3f5f4b
Ameloko Emmanuel <164350008+AmelokoEmmanuel@users.noreply.github.com>
2024-07-13 12:31:25 +0100

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
+    bucket = "emmanu123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
